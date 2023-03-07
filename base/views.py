fc80edd68cb3c668787609b65afe8ab4a9ae9c9c
Rantoryu <dominikmigrala@gmail.com>
2023-03-07 14:02:31 +0100

    Initial Commit

---
+++ base/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.contrib import messages
+
+from .forms import 
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'base/home.html')
+
+def posts(request):
+    return render(request, 'base/posts.html')
+
+def post(request):
+    return render(request, 'base/post.html')
+
+def profile(request):
+    return render(request, 'base/profile.html')
