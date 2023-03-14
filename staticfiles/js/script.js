cb3d290f4c92069f38436ab8f0cdab5197e42cf7
Rantoryu <dominikmigrala@gmail.com>
2023-03-14 18:50:25 +0100

    Added Forms and ACL changes

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,36 +1,40 @@
-console.log('Script is working')
+console.log('Its working')
 
 let theme = localStorage.getItem('theme')
 
 if(theme == null){
-    setTheme('light')
+	setTheme('light')
 }else{
-    setTheme(theme)
+	setTheme(theme)
 }
 
 let themeDots = document.getElementsByClassName('theme-dot')
 
-for(var i=0; themeDots.length > i; i++){
-    themeDots[i].addEventListener('click', function(){
-        let mode = this.dataset.mode
-        console.log('Theme changed', mode)
-        setTheme(mode)
-    })
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
 }
 
 function setTheme(mode){
-    if(mode == 'light'){
-        document.getElementById('theme-style').href = 'default.css'
-    }
-    if(mode == 'blue'){
-        document.getElementById('theme-style').href = 'blue.css'
-    }
-    if(mode == 'green'){
-        document.getElementById('theme-style').href = 'green.css'
-    }
-    if(mode == 'purple'){
-        document.getElementById('theme-style').href = 'purple.css'
-    }
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = static + '/default.css'
+	}
 
-    localStorage.setItem('theme', mode)
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = static + '/blue.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = static + '/green.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = static + '/purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
 }
