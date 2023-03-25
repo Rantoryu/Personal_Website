e5b0cfba7b4891cf14cdbdfda52c9c250877dcf2
Rantoryu <dominikmigrala@gmail.com>
2023-03-25 23:46:54 +0100

    Form

--- base/forms.py
+++ base/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from captcha.fields import ReCaptchaField
 from .models import Post, Profile
 
 
